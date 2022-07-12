fd9d62084dcf63eaac1b5472cdeba25a7cc9fb8b
froloffw7 <69122526+froloffw7@users.noreply.github.com>
2022-07-13 00:12:55 +0200

    Add some sanity checks

--- Test code/Scope_emulator/sd_blk.c
+++ Test code/Scope_emulator/sd_blk.c
@@ -6,14 +6,18 @@
 
 
 bool blk_is_writable(FILE *blk) {
-	return true;
+	if (blk) return true;
 }
 
 bool blk_is_inserted(FILE *blk) {
-	return true;
+	if (blk) return true;
 }
 
 void blk_get_geometry(FILE *blk, uint64_t *sect) {
+	if (!blk) {
+		*sect = 0;
+		return;
+	}
 	int64_t ofs;
 	ofs = ftell(blk);
 	fseek(blk, 0, SEEK_END);
@@ -23,12 +27,14 @@ void blk_get_geometry(FILE *blk, uint64_t *sect) {
 }
 
 int32_t blk_pread(FILE *blk, uint64_t addr, uint8_t *data, uint32_t len) {
+	if (!blk) return -1;
 	f1c100s_log_mask(LOG_GUEST_TRACE, "blk_read: %llx[%d]\n", addr, len);
 	fseek(blk, addr, SEEK_SET);
 	fread(data, 1, len, blk);
 	return len;
 }
 int32_t blk_pwrite(FILE *blk, uint64_t addr, uint8_t *data, uint32_t len, int u) {
+	if (!blk) return -1;
 	f1c100s_log_mask(LOG_GUEST_TRACE, "blk_write: %llx[%d]\n", addr, len);
 	fseek(blk, addr, SEEK_SET);
 	fwrite(data, 1, len, blk);
